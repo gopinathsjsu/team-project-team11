fc14f817de8a9c5e19a2d82dc4220f3eba9fff97
Naren Sulegai <86naren@gmail.com>
2021-04-04 18:47:57 -0700

    Adding more routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,23 +9,26 @@ import Landing from "./components/Landing";
 import CustomerSignup from "./components/CustomerSignup";
 import CustomerLogin from "./components/CustomerLogin";
 import AdminLogin from "./components/AdminLogin";
+import CustomerHome from "./components/CustomerHome";
+import AdminHome from "./components/AdminHome";
 
 export default function App() {
+    const routes = [
+        ["/", Landing],
+        ["/customerLogin", CustomerLogin],
+        ["/customerHome", CustomerHome],
+        ["/customerSignup", CustomerSignup],
+        ["/adminLogin", AdminLogin],
+        ["/adminHome", AdminHome],
+    ];
     return (
         <Router>
             <Switch>
-                <Route path="/" exact={true}>
-                    <Landing/>
-                </Route>
-                <Route path="/customerlogin" exact={true}>
-                    <CustomerLogin/>
-                </Route>
-                <Route path="/customerSignup" exact={true}>
-                    <CustomerSignup/>
-                </Route>
-                <Route path="/adminlogin" exact={true}>
-                    <AdminLogin/>
-                </Route>
+                {routes.map((r, i) => {
+                    return <Route path={r[0]} exact={true} key={r[0]}>
+                        {r[1]}
+                    </Route>
+                })}
             </Switch>
         </Router>
     );
