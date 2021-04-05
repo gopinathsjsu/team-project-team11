a23a05362e60d3e6bf6040d543ea3377b899220c
Naren Sulegai <86naren@gmail.com>
2021-04-04 19:05:21 -0700

    Landing page

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -3,9 +3,22 @@ import {Link} from "react-router-dom";
 
 const Landing = props => {
     return (
-        <div>
-            <Link to="/customerLogin">Customer Login</Link>
-            <Link to="/customerSignup">Customer Signup</Link>
+        <div className="full">
+            <h2 className="center">Welcome to UnitedBank</h2>
+            <div className="flex flex-align-center">
+                <div className="flex-full center">
+                    <div>Do you have an account?</div>
+                    <Link to="/customerLogin">Login here</Link>
+                </div>
+                <div className="flex-full center">
+                    <div>Do you want to create an account?</div>
+                    <Link to="/customerSignup">Signup here</Link>
+                </div>
+                <div className="flex-full center">
+                    <div>Are you a bank admin?</div>
+                    <Link to="/adminLogin">Login here</Link>
+                </div>
+            </div>
         </div>
     );
 };
