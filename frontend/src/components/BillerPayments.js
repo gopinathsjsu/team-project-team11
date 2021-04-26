a98fc2470943b242272ee5b16e5ca1a911512317
AKASH SINGHAL <akashsinghal@AKASHs-MacBook-Pro.local>
2021-04-26 12:39:22 -0700

    indentation

--- frontend/src/components/BillerPayments.js
+++ frontend/src/components/BillerPayments.js
@@ -31,34 +31,38 @@ const BillerPayments = () => {
   const loadCustomerTransactionPage = () => {
     if (customer) {
       return customer.accounts.length > 0
-        ? (<div>
-          <h2>Transfer to external accounts</h2>
-          <div className="flex-column">
-            <div className="medium-margin-top flex">
-              <div className="fixed-width-tags medium-margin-right bolder-text">From&nbsp;&nbsp;</div>
-              <select ref={extFromRef}>
-                {customer.accounts.filter((a) => a.isActive).map((a) => {
-                  return <option key={a._id} value={a._id}>{a._id} (${a.balance})</option>;
-                })}
-              </select>
-            </div>
-            <div className="medium-margin-top flex">
-              <div className="fixed-width-tags medium-margin-right  bolder-text">Select Biller&nbsp;&nbsp;</div>
-              <div><select ref={extToRef}>
-                {[{ name: 'Electricity' }, { name: 'Water' }].map((e, i) => {
-                  return <option key={i}>{e.name}</option>;
-                })}
-              </select></div>
-            </div>
+        ? (
+          <div>
+            <h2>Transfer to external accounts</h2>
+            <div className="flex-column">
+              <div className="medium-margin-top flex">
+                <div className="fixed-width-tags medium-margin-right bolder-text">From&nbsp;&nbsp;</div>
+                <select ref={extFromRef}>
+                  {customer.accounts.filter((a) => a.isActive).map((a) => {
+                    return <option key={a._id} value={a._id}>{a._id} (${a.balance})</option>;
+                  })}
+                </select>
+              </div>
+              <div className="medium-margin-top flex">
+                <div className="fixed-width-tags medium-margin-right  bolder-text">Select Biller&nbsp;&nbsp;</div>
+                <div><select ref={extToRef}>
+                  {[{ name: 'Electricity' }, { name: 'Water' }].map((e, i) => {
+                    return <option key={i}>{e.name}</option>;
+                  })}
+                </select></div>
+              </div>
 
-            <div className="medium-margin-top flex">
-              <div className="fixed-width-tags medium-margin-right bolder-text">Amount&nbsp;&nbsp;</div>
-              <input type="number" ref={extAmountRef} placeholder="Amount" />
+              <div className="medium-margin-top flex">
+                <div className="fixed-width-tags medium-margin-right bolder-text">Amount&nbsp;&nbsp;</div>
+                <input type="number" ref={extAmountRef} placeholder="Amount" />
+              </div>
+              <div><button className="large-margin-top button large-margin-left  fixed-width-tags" onClick={handleOnExternalTransfer}>Transfer</button></div>
             </div>
-            <div><button className="large-margin-top button large-margin-left  fixed-width-tags" onClick={handleOnExternalTransfer}>Transfer</button></div>
           </div>
-        </div>) : (<h2>You dont have any account added yet.
-          Please request for an account to use this feature</h2>);
+        ) : (
+          <h2>You dont have any account added yet.
+            Please request for an account to use this feature</h2>
+        );
     }
     return 'Loading your profile';
   };
