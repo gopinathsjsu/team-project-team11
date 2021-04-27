d26cef42559b5f759e70b9c3f1d4de551459340e
Neha Poonia <neha.poonia@sjsu.edu>
2021-04-26 21:26:49 -0700

    Data Grid

--- frontend/src/components/AccountAdjustments.js
+++ frontend/src/components/AccountAdjustments.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { DataGrid, GridColDef, GridCellParams } from '@material-ui/data-grid';
 import { getAccounts, updateAccountBalance } from '../util/fetch/api';
 
 const AccountAdjustments = () => {
@@ -25,52 +26,74 @@ const AccountAdjustments = () => {
     await updateAccountBalance({ _id: account._id, balance: account.balance });
     alert(`Updated balance to ${account.balance}`);
   };
-
+  const columns = [
+    { field: 'id', headerName: 'Account ID', flex: 5 },
+    { field: 'balance', headerName: 'Balance', flex: 5 },
+    { field: 'type', headerName: 'Account Type', flex: 5 },
+    { field: 'name', headerName: 'Customer Name', flex: 5 },
+    { field: 'email', headerName: 'Customer Email', flex: 5 },
+    {
+      field: '',
+      headerName: 'btn',
+      renderCell: (params: GridCellParams) => (
+        <button style={{ marginLeft: 16, color: 'blue' }}>Update balance</button>
+      ),
+    },
+  ];
   return (
-    <div className="body">
-      <h2>Customer account</h2>
-      <div>
-        <div>{accounts.length === 0 && 'No accounts to show'}</div>
-        <table className="table">
-          <thead>
-            <tr>
-              <td>Account ID</td>
-              <td>Balance</td>
-              <td>Account Type</td>
-              <td>Customer Name</td>
-              <td>Customer Email</td>
-              <td>&nbsp;</td>
-            </tr>
-          </thead>
-          <tbody>
-            {accounts.map((account) => {
-              return (
-                <tr key={account._id}>
-                  <td>{account._id}</td>
-                  <td>
-                    <input type="number" value={account.balance}
-                      onChange={(e) => {
-                        updateBalance(account, e.target.value);
-                      }} />
-                  </td>
-                  <td>{account.type}</td>
-                  <td>{account.customer.name}</td>
-                  <td>{account.customer.email}</td>
-                  <td>
-                    <button onClick={() => {
-                      saveBalance(account);
-                    }}>
-                      Update balance
-                    </button>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
+    <div style={{ height: 400, width: '100%' }}>
+      <DataGrid
+        columns={columns}
+        rows={accounts}
+        pageSize={10}
+      />
     </div>
   );
+  // return (
+  //   <div className="body">
+  //     <h2>Customer account</h2>
+  //     <div>
+  //       <div>{accounts.length === 0 && 'No accounts to show'}</div>
+  //       <table className="table">
+  //         <thead>
+  //           <tr>
+  //             <td>Account ID</td>
+  //             <td>Balance</td>
+  //             <td>Account Type</td>
+  //             <td>Customer Name</td>
+  //             <td>Customer Email</td>
+  //             <td>&nbsp;</td>
+  //           </tr>
+  //         </thead>
+  //         <tbody>
+  //           {accounts.map((account) => {
+  //             return (
+  //               <tr key={account._id}>
+  //                 <td>{account._id}</td>
+  //                 <td>
+  //                   <input type="number" value={account.balance}
+  //                     onChange={(e) => {
+  //                       updateBalance(account, e.target.value);
+  //                     }} />
+  //                 </td>
+  //                 <td>{account.type}</td>
+  //                 <td>{account.customer.name}</td>
+  //                 <td>{account.customer.email}</td>
+  //                 <td>
+  //                   <button onClick={() => {
+  //                     saveBalance(account);
+  //                   }}>
+  //                     Update balance
+  //                   </button>
+  //                 </td>
+  //               </tr>
+  //             );
+  //           })}
+  //         </tbody>
+  //       </table>
+  //     </div>
+  //   </div>
+  // );
 };
 
 export default AccountAdjustments;
--- frontend/src/components/AccountRequests.js
+++ frontend/src/components/AccountRequests.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { DataGrid, GridColDef, GridCellParams } from '@material-ui/data-grid';
 import {
   approveAccountRequest, fileUrl, getAccountRequests, getAccounts,
 } from '../util/fetch/api';
@@ -23,56 +24,83 @@ const AccountRequests = () => {
       alert('Account request has been approved');
     }
   };
-
+  const columns: GridColDef[] = [
+    { field: 'id', headerName: 'Account ID', flex: 5 },
+    { field: 'balance', headerName: 'Balance', flex: 5 },
+    { field: 'type', headerName: 'Account Type', flex: 5 },
+    { field: 'name', headerName: 'Customer Name', flex: 5 },
+    { field: 'email', headerName: 'Customer Email', flex: 5 },
+    {
+      field: '',
+      headerName: 'btn',
+      renderCell: (params: GridCellParams) => (
+        <button style={{ marginLeft: 16, color: 'blue' }}>Approve</button>
+      ),
+    },
+  ];
+  console.log({accountRequests});
   return (
-    <div className="body">
-      <h2>Account requests</h2>
-      <div>
-        <div>{accountRequests.length === 0 && 'You have no new requests to approve'}</div>
-        <table className="table">
-          <thead>
-            <tr>
-              <td>Account ID</td>
-              <td>Balance</td>
-              <td>Account Type</td>
-              <td>Customer Name</td>
-              <td>Customer Email</td>
-              <td>&nbsp;</td>
-            </tr>
-          </thead>
-          <tbody>
-            {accountRequests.map((account) => {
-              return (
-                <React.Fragment key={account._id}>
-                  <tr>
-                    <td>{account._id}</td>
-                    <td>${account.balance}</td>
-                    <td>{account.type}</td>
-                    <td>{account.customer.name}</td>
-                    <td>{account.customer.email}</td>
-                    <td>
-                      <button onClick={(acc) => { approveRequest(account); }}>
-                        Approve
-                      </button>
-                    </td>
-                  </tr>
-                  <tr className="border-bottom">
-                    <td colSpan={6}>
-                      <div className="uploaded-file medium-margin-top">
-                        {account.files.map((f) => {
-                          return <img key={f} src={fileUrl(f)} alt={fileUrl(f)} />;
-                        })}
-                      </div>
-                    </td>
-                  </tr>
-                </React.Fragment>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
+    
+
+    <div style={{ height: 400, width: '100%' }}>
+      <DataGrid
+        columns={columns}
+        
+        rows={accountRequests}
+        pageSize={10}
+      />
     </div>
   );
+
+  // return (
+  //   <div className="body">
+  //     <h2>Account requests</h2>
+  //     <div>
+  //       <div>{accountRequests.length === 0 && 'You have no new requests to approve'}</div>
+  //       <table className="table">
+  //         <thead>
+  //           <tr>
+  //             <td>Account ID</td>
+  //             <td>Balance</td>
+  //             <td>Account Type</td>
+  //             <td>Customer Name</td>
+  //             <td>Customer Email</td>
+  //             <td>&nbsp;</td>
+  //           </tr>
+  //         </thead>
+  //         <tbody>
+  //           {accountRequests.map((account) => {
+  //             return (
+  //               <React.Fragment key={account._id}>
+  //                 <tr>
+  //                   <td>{account._id}</td>
+  //                   <td>${account.balance}</td>
+  //                   <td>{account.type}</td>
+  //                   <td>{account.customer.name}</td>
+  //                   <td>{account.customer.email}</td>
+  //                   <td>
+  //                     <button onClick={(acc) => { approveRequest(account); }}>
+  //                       Approve
+  //                     </button>
+  //                   </td>
+  //                 </tr>
+  //                 <tr className="border-bottom">
+  //                   <td colSpan={6}>
+  //                     <div className="uploaded-file medium-margin-top">
+  //                       {account.files.map((f) => {
+  //                         return <img key={f} src={fileUrl(f)} alt={fileUrl(f)} />;
+  //                       })}
+  //                     </div>
+  //                   </td>
+  //                 </tr>
+  //               </React.Fragment>
+  //             );
+  //           })}
+  //         </tbody>
+  //       </table>
+  //     </div>
+  //   </div>
+  // );
 };
 
 export default AccountRequests;
