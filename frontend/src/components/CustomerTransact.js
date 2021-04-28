99e16877910160ae854a75a34f181ec3e023a605
AKASH SINGHAL <akash.singhal67@yahoo.com>
2021-04-28 10:56:53 -0700

    UI changes for transact page in case user does not have any account added or active

--- frontend/src/components/CustomerTransact.js
+++ frontend/src/components/CustomerTransact.js
@@ -37,47 +37,57 @@ const CustomerTransact = () => {
     })();
   }, []);
 
+  const loadCustomerTransactionPage = () => {
+    if (customer) {
+      return customer.accounts.filter((a) => a.isActive) > 0
+        ? (
+          <div>
+            <h2>Transact between accounts</h2>
+            <div className="flex-column">
+              <div className="medium-margin-top flex">
+                <div className="fixed-width-tags medium-margin-right bolder-text">Is this an exteral transfer?&nbsp;&nbsp;</div>
+                <input type="checkbox" value={isExternalTransfer} onClick={(event) => {
+                  setIsExternalTransfer(!isExternalTransfer);
+                }} />
+              </div>
+              <div className="medium-margin-top flex">
+                <div className="fixed-width-tags medium-margin-right bolder-text">From&nbsp;&nbsp;</div>
+                <select ref={fromRef}>
+                  {customer.accounts.filter((a) => a.isActive).map((a) => {
+                    return <option key={a._id} value={a._id}>{a._id} (${a.balance})</option>;
+                  })}
+                </select>
+              </div>
+              <div className="medium-margin-top flex">
+                <div className="fixed-width-tags medium-margin-right bolder-text">To&nbsp;&nbsp;</div>
+                {isExternalTransfer ? (
+                  <input type="text" ref={toRef} placeholder="Account Number" />)
+                  : (
+                    <select ref={toRef}>
+                      {customer.accounts.filter((a) => a.isActive).map((a) => {
+                        return <option key={a._id} value={a._id}>{a._id} (${a.balance})</option>;
+                      })}
+                    </select>
+                  )}
+              </div>
+              <div className="medium-margin-top flex">
+                <div className="fixed-width-tags medium-margin-right bolder-text">Amount&nbsp;&nbsp;</div>
+                <input type="number" ref={amountRef} placeholder="Amount" />
+              </div>
+              <div><button className="large-margin-top button large-margin-left  fixed-width-tags" onClick={handleOnTransfer}>Transfer</button></div>
+            </div>
+          </div>
+        ) : (
+          <h2>You dont have any account added yet.
+            Please request for an account to use this feature</h2>
+        );
+    }
+    return 'Loading your profile';
+  };
+
   return (
     <div className="body">
-      {customer ? (
-        <>
-          <h2>Transact between accounts</h2>
-          <div className="flex-column">
-            <div className="medium-margin-top flex">
-              <div className="fixed-width-tags medium-margin-right bolder-text">Is this an exteral transfer?&nbsp;&nbsp;</div>
-              <input type="checkbox" value={isExternalTransfer} onClick={(event) => {
-                setIsExternalTransfer(!isExternalTransfer);
-              }} />
-            </div>
-            <div className="medium-margin-top flex">
-              <div className="fixed-width-tags medium-margin-right bolder-text">From&nbsp;&nbsp;</div>
-              <select ref={fromRef}>
-                {customer.accounts.filter((a) => a.isActive).map((a) => {
-                  return <option key={a._id} value={a._id}>{a._id} (${a.balance})</option>;
-                })}
-              </select>
-            </div>
-            <div className="medium-margin-top flex">
-              <div className="fixed-width-tags medium-margin-right bolder-text">To&nbsp;&nbsp;</div>
-              {isExternalTransfer ? (
-                <input type="text" ref={toRef} placeholder="Account Number" />)
-                : (
-                  <select ref={toRef}>
-                    {customer.accounts.filter((a) => a.isActive).map((a) => {
-                      return <option key={a._id} value={a._id}>{a._id} (${a.balance})</option>;
-                    })}
-                  </select>
-                )}
-            </div>
-            <div className="medium-margin-top flex">
-              <div className="fixed-width-tags medium-margin-right bolder-text">Amount&nbsp;&nbsp;</div>
-              <input type="number" ref={amountRef} placeholder="Amount" />
-            </div>
-            <div><button className="large-margin-top button large-margin-left  fixed-width-tags" onClick={handleOnTransfer}>Transfer</button></div>
-          </div>
-        </>
-      ) : 'Loading your profile'}
-
+      {loadCustomerTransactionPage()}
     </div>
   );
 };
