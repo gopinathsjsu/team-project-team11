983b664515d9b59625b6143c43edc08110c1daf6
Naren Sulegai <86naren@gmail.com>
2021-05-09 11:12:40 -0700

    Seach by account number

--- frontend/src/components/CustomerTransactions.js
+++ frontend/src/components/CustomerTransactions.js
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { currentCustomer, getTransactions } from '../util/fetch/api';
 
 const CustomerTransactions = () => {
   const [customer, setCustomer] = useState(null);
   const [transactions, setTransactions] = useState([]);
+  const searchRef = useRef(null);
 
   useEffect(() => {
     (async () => {
@@ -12,11 +13,26 @@ const CustomerTransactions = () => {
     })();
   }, []);
 
+  const handleOnSearch = async () => {
+    const text = searchRef.current.value;
+    let transactions = await getTransactions();
+    if (text.length) {
+      transactions = transactions.filter((t) => {
+        return t.from._id.match(text);
+      });
+    }
+    setTransactions(transactions);
+  };
+
   return (
     <div className="body">
       {customer ? (
         <>
           <h2>Your transactions</h2>
+          <div>
+            <input type="text" ref={searchRef} placeholder="Search account number" />
+            <button className="button" onClick={handleOnSearch}>Search account</button>
+          </div>
           <table className="table">
             <thead>
               <tr>
