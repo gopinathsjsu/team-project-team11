4fcaab9c3757958b63942417805703587094b9f1
Naren Sulegai <86naren@gmail.com>
2021-05-09 10:37:34 -0700

    Styling landing page

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -10,15 +10,15 @@ const Landing = () => {
         </div>
       </div>
       <div className="flex flex-align-center medium-margin-top">
-        <div className="flex-full center">
+        <div className="flex-full center landing-block">
           <div>Do you have an account?</div>
           <Link to={window.appRoutes.customerLogin}>Login here</Link>
         </div>
-        <div className="flex-full center">
+        <div className="flex-full center landing-block">
           <div>Do you want to create an account?</div>
           <Link to={window.appRoutes.customerSignup}>Signup here</Link>
         </div>
-        <div className="flex-full center">
+        <div className="flex-full center landing-block">
           <div>Are you a bank admin?</div>
           <Link to={window.appRoutes.adminLogin}>Login here</Link>
         </div>
