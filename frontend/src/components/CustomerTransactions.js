84f00c75605fe864d53bd67f2336e8f2b3322fa8
Naren Sulegai <86naren@gmail.com>
2021-05-15 09:08:13 -0700

    Filter transactions from 18 months

--- frontend/src/components/CustomerTransactions.js
+++ frontend/src/components/CustomerTransactions.js
@@ -5,6 +5,8 @@ const CustomerTransactions = () => {
   const [customer, setCustomer] = useState(null);
   const [transactions, setTransactions] = useState([]);
   const searchRef = useRef(null);
+  const d = new Date();
+  const eighteenMonthsBack = d.setMonth(d.getMonth() - 18);
 
   useEffect(() => {
     (async () => {
@@ -32,10 +34,19 @@ const CustomerTransactions = () => {
       {customer ? (
         <>
           <h2>Your transactions</h2>
-          <div>
-            <input type="text" ref={searchRef} placeholder="Search account number" />
-            <button className="button small-margin-left" onClick={handleOnSearch}>Search account</button>
+          <div className="flex flex-justify-content-space-between flex-align-center">
+            <div>
+              <input type="text" ref={searchRef} placeholder="Search account number"
+                className="no-margin-top" />
+              <button className="button small-margin-left no-margin-top" onClick={handleOnSearch}>Search
+                account
+              </button>
+            </div>
+            <div className="small-margin-top">
+              Transactions in last 18 months
+            </div>
           </div>
+
           <table className="table small-margin-top">
             <thead>
               <tr>
@@ -52,17 +63,21 @@ const CustomerTransactions = () => {
                 <td className="center" colSpan={5}>No transactions yet</td>
               </tr>
               )}
-              {transactions.map((t, i) => {
-                return (
-                  <tr key={i}>
-                    <td>{displayAccount(t.from)}</td>
-                    <td>{t.isExternal ? t.toExternal : displayAccount(t.to)}</td>
-                    <td>{t.description}</td>
-                    <td>${t.amount}</td>
-                    <td>{new Date(t.createdAt).toDateString()}</td>
-                  </tr>
-                );
-              })}
+              {transactions
+                .filter((t) => {
+                  return new Date(t.createdAt) > eighteenMonthsBack;
+                })
+                .map((t, i) => {
+                  return (
+                    <tr key={i}>
+                      <td>{displayAccount(t.from)}</td>
+                      <td>{t.isExternal ? t.toExternal : displayAccount(t.to)}</td>
+                      <td>{t.description}</td>
+                      <td>${t.amount}</td>
+                      <td>{new Date(t.createdAt).toDateString()}</td>
+                    </tr>
+                  );
+                })}
             </tbody>
           </table>
 
